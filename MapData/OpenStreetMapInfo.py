297201f7b9519147400b56feda97b1fd458262ee
Connor Bramhall <41408864+Connorwb@users.noreply.github.com>
2022-09-28 17:24:55 -0400

    Imported Python harvested data into Unity

--- MapData/OpenStreetMapInfo.py
+++ MapData/OpenStreetMapInfo.py
@@ -6,18 +6,18 @@ overpass = Overpass()
 nodeIDs = {}
 doublesIDs = {}
 result = overpass.query(overpassQueryBuilder(area=areaId, elementType='way', selector='"highway"~"path|foot|crossing|footway|sidewalk"'))
-Downloads = open("..\\SafeStride\\Assets+"
-                 "+\\Data\\Intersections.txt" , "w+")
+Downloads = open("..\\SafeStride\\Assets\\Data\\Intersections.txt", "w+")
 for way in result.elements():
     for node in way.nodes():
         if node.id() in nodeIDs.keys():
             doublesIDs.update({node.id(): str(node.lat()) + "," + str(node.lon()) + "," + nodeIDs[node.id()]})
             del nodeIDs[node.id()]
-            print("Found intersection " + str(node.id()) + " at " + str(node.lat()) + "," + str(node.lon()))
+            #print("Found intersection " + str(node.id()) + " at " + str(node.lat()) + "," + str(node.lon()))
         elif node.id() in doublesIDs.keys():
             doublesIDs[node.id()] = doublesIDs[node.id()] + "," + str(way.id())
         else:
             nodeIDs.update({node.id(): str(way.id())})
-for item in doublesIDs.items():
-    Downloads.write(item + "\n")
+for key, value in doublesIDs.items():
+    print(value)
+    Downloads.write(value + "\n")
 print("ALL DONE!")
